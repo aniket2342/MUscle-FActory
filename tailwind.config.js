4849d066c5f42b783dad575bdb1ed2dca549f888
aniket2342 <133370554+aniket2342@users.noreply.github.com>
2024-01-14 17:50:23 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        libre:['Dancing Script', cursive, 'Genos', sans-serif,'Poppins', sans-serif]
+      }
+    },
+  },
+  plugins: [],
+}
+
